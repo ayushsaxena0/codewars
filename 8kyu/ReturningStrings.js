d50d934d9a75e1b5f16a8cbd4a91b480aab0cc50
Ayush <ayushsaxena917@gmail.com>
2024-01-01 19:07:29 +0530

    Question Of the Day

---
+++ 8kyu/ReturningStrings.js
@@ -0,0 +1,11 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// PREP
+// parameter -> should be string, can be anything
+// return -> simple return only
+// example -> greet("Ryan")-> "Hello, Ryan how are you doing today?"
+// pseudo code -> get the input, insert it in string using template literals, return the string
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
