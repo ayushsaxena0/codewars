f92ddb2ee3e1f427efe2989387faeba826f2323a
Ayush <ayushsaxena917@gmail.com>
2024-03-01 22:15:36 +0530

    Codewars ques1

---
+++ 8kyu/HelloWorld.js
@@ -0,0 +1,9 @@
+/*Make a simple function called greet that returns the most-famous "hello world!". */
+
+// PREP
+// parameters -> none
+// return -> string, normal return
+// example -> greet() -> 'hello world!'
+// pseudo code -> create func greet, return string "hello world!"
+
+const greet = (_) => "hello world!";
