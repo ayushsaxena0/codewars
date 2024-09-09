b40af91814c84c71af9b80154f05c442f367ce80
Ayush <ayushsaxena917@gmail.com>
2024-09-09 14:32:22 +0530

    Add Coding Challenge Question(09 Sept)

---
+++ 8kyu/KataExample.js
@@ -0,0 +1,11 @@
+/*This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites 1,000 times. */
+
+// add the value "codewars" to the websites array 1,000 times
+let websites = [];
+let i = 1;
+while (i <= 1000) {
+  websites.push("codewars");
+  i++;
+}
